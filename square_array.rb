c3514828b8970f3ce1a687832680db2e2a4a74be
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-08-24 14:13:17 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   #array.each {|x| puts Math.sqrt(x)}
   #array
   
-  new_array = array.each do |x| x*x 
-  new_array
+  result = array.each do |x| x*x 
+  result
  
  end
    
